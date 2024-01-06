a319f305dc912c65a1d59cb3739686f4afe7b39c
raghavverma.dev@gmail.com <raghavverma.dev@gmail.com>
2024-01-07 00:13:59 +0530

    Initial commit

---
+++ lib/utils/config.dart
@@ -0,0 +1,25 @@
+const imageUrl = "assets/images/magCover.png";
+//const dummyAddress = "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199";
+const dummyAddress = "0xa52Dc1E49B6F58e5EDC84E2C8dEae6325F0321f4";
+
+enum Source { home, myNfts, profileCillectibles }
+
+//Contract configurations
+const contractAddress = "0x7b3E60882336dDE6b00B9419460C1118de671194";
+const url = "https://sepolia.infura.io/v3/57ebb58ad52c4e4fa8562695d460d26b"; //rpc url
+const wsUrl = "wss://sepolia.infura.io/v3/57ebb58ad52c4e4fa8562695d460d26b"; //websocket url
+// const dummyPrivateKey =
+//     "0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e";
+const dummyPrivateKey =
+    "b580d6cdf3812fbb0c1cd48bf6f7e96a9508c79db7e9f9ec58243a1279360957";
+const genesisAddress = "0x0000000000000000000000000000000000000000";
+
+//Pinata configurations
+const pinataUrl = "https://api.pinata.cloud";
+const pinEndpoint = "/pinning/pinFileToIPFS";
+const apiKey = "f83f67660f94493c49c5";
+const apiSecret =
+    "220f40e9f0e538ba2e04f09747057bdd679bbe849a56c64de6158dc1a96bcdfc";
+const jwt =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJlNTZhODJiNi1iYmY5LTRiNDgtYmViNy0wNTA4NzY1OGRjYjkiLCJlbWFpbCI6Im1vYnRlcmVzdEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6IkZSQTEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX0seyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiZjgzZjY3NjYwZjk0NDkzYzQ5YzUiLCJzY29wZWRLZXlTZWNyZXQiOiIyMjBmNDBlOWYwZTUzOGJhMmUwNGYwOTc0NzA1N2JkZDY3OWJiZTg0OWE1NmM2NGRlNjE1OGRjMWE5NmJjZGZjIiwiaWF0IjoxNjgwMTgzODc2fQ.Z8RDxTTFzDuMsEOMWpDWExXS8uAyy0OqqF15xeSnn_A";
+const pinataGateway = "https://gateway.pinata.cloud/ipfs/";
